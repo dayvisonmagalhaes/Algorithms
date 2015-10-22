c09a6240ac23b907c5e039a72d9eed16b31f84b2
Davidson Sestaro <davidson.sestaro@fcamara.com.br>
2015-10-22 10:00:21 -0200

    Inicio do desenvolvimento do C4.5

---
+++ C4.5 Algorithm/src/br/com/c45/Start.java
@@ -0,0 +1,12 @@
+package br.com.c45;
+
+public class Start {
+
+	public static void main(String[] args) {
+		//Caminho para a pasta onde será lido o arquivo com a base de dados
+		String path = "C:\\Users\\davidson.sestaro\\Dropbox\\IA\\";
+
+		
+	}
+
+}
