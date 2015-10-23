3346cf91525d0c11b168c0352892c29cb4ea3971
Davidson Sestaro <davidson.sestaro@fcamara.com.br>
2015-10-23 09:06:23 -0200

    Implementacao da Disntancia Euclidiana

--- KMeans/src/br/com/kmeans/evaluate/EuclidianDistance.java
+++ KMeans/src/br/com/kmeans/evaluate/EuclidianDistance.java
@@ -1,10 +1,10 @@
 package br.com.kmeans.evaluate;
 
-import br.com.kmeans.centers.Centroide;
+import br.com.kmeans.centroid.Centroid;
 import br.com.kmeans.record.Record;
 
 public class EuclidianDistance {
-	public static double calculaDistance(Centroide center, Record record) {
+	public static double calculaDistance(Centroid center, Record record) {
 		double distance = 0;
 		
 		for(int i = 0; i < center.getCenters().size(); i++) {
